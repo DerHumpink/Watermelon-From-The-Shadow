69b515e68cc49bb354b0703a88e21fe4bddbaa13
DerHumpink <steinbock@threaks.com>
2016-04-23 17:27:05 +0200

    Implemented shadow system

---
+++ WatermelonProject/Assets/Scripts/ShadowCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowCamera : MonoBehaviour {
+	
+	void Update ()
+	{
+		transform.position = CameraController.Instance.transform.position;
+	}
+}
---
+++ WatermelonProject/Assets/Scripts/ShadowController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Util;
+
+public class ShadowController : Singleton<ShadowController>
+{
+	[SerializeField] private Transform _sunBase;
+
+	public void Update()
+	{
+		SetSunDirection(Time.time*30);
+	}
+
+	public void SetSunDirection(float direction)
+	{
+		Quaternion q = Quaternion.Euler(0,0,direction);
+		Vector3 pos = Vector3.up*20;
+		_sunBase.position = q*pos;
+	}
+}
