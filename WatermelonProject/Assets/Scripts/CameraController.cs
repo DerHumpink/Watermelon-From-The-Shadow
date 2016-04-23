fbc441894f0e76e5eb020f2e441e39f424933ab1
DerHumpink <steinbock@threaks.com>
2016-04-23 16:11:49 +0200

    Created Main Camera.prefab stuff

--- WatermelonProject/Assets/Scripts/CameraController.cs
+++ WatermelonProject/Assets/Scripts/CameraController.cs
@@ -1,12 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
 using Util;
 
-public class CameraController : Singleton<CameraController> {
+public class CameraController : Singleton<CameraController>
+{
+	[SerializeField] private float _scrollSpeed=2;
+	[SerializeField] private Vector3 _playerOffset=Vector2.up;
 
-	
+	public List<CameraTrigger> CameraTriggers=new List<CameraTrigger>();
+
+
 	void Update ()
 	{
-		transform.position = Movement.Instance.transform.position;
+		var playerPos = Movement.Instance.transform.position;
+		var focusPoint= playerPos+ _playerOffset;
+
+		if (CurrentCameraTrigger)
+		{
+			focusPoint = Vector3.Lerp(playerPos, CurrentCameraTrigger.transform.position, CurrentCameraTrigger.LerpFactor);
+		}
+
+		transform.position = Vector3.Lerp(transform.position,focusPoint,_scrollSpeed*Time.deltaTime);
+	}
+
+	public CameraTrigger CurrentCameraTrigger
+	{
+		get
+		{
+			float playerPos = Movement.Instance.transform.position.x;
+			return CameraTriggers.FirstOrDefault(c => c.Range.Contains(playerPos));
+		}
 	}
 }
--- WatermelonProject/Assets/Scripts/CameraTrigger.cs
+++ WatermelonProject/Assets/Scripts/CameraTrigger.cs
@@ -2,14 +2,34 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraTrigger : MonoBehaviour {
+public class CameraTrigger : MonoBehaviour
+{
+	[SerializeField] public RangeFloat Range;
+	[SerializeField] public float LerpFactor;
 
-	//[SerializeField]
-
-
-	[Serializable]
-	public struct RangeFloat
+	private void Start()
 	{
-		public float Min, Max;
+		CameraController.Instance.CameraTriggers.Add(this);
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = new Color(0,0.2f,0.8f,0.4f);
+		Vector3 center = Vector3.right*Range.Center;
+		Vector3 size = new Vector3(Range.Size, 10, 0);
+		Gizmos.DrawCube(center, size);
+	}
+}
+
+[Serializable]
+public struct RangeFloat
+{
+	public float Min, Max;
+	public float Center { get { return Mathf.Lerp(Min, Max, 0.5f); } }
+	public float Size { get { return Max-Min;} }
+
+	public bool Contains(float playerPos)
+	{
+		return playerPos > Min && playerPos < Max;
 	}
 }
