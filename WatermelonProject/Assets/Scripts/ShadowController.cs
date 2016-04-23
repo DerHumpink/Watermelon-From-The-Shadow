4fb7b2bef5ff08532709cf79b5f6290487ea52cf
DerHumpink <steinbock@threaks.com>
2016-04-23 17:53:40 +0200

    fixed shadow angel

--- WatermelonProject/Assets/Scripts/ShadowController.cs
+++ WatermelonProject/Assets/Scripts/ShadowController.cs
@@ -6,15 +6,11 @@ public class ShadowController : Singleton<ShadowController>
 {
 	[SerializeField] private Transform _sunBase;
 
-	public void Update()
-	{
-		SetSunDirection(Time.time*30);
-	}
-
 	public void SetSunDirection(float direction)
 	{
 		Quaternion q = Quaternion.Euler(0,0,direction);
 		Vector3 pos = Vector3.up*20;
-		_sunBase.position = q*pos;
+		Debug.Log(direction);
+		_sunBase.position = transform.position+q*pos;
 	}
 }
