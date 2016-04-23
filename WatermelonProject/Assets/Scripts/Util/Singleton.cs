3dba67efeedbe1765fb5a473a53c45bc08959284
DerHumpink <steinbock@threaks.com>
2016-04-23 15:00:36 +0200

    Added basic stuff, DOTween plugin

---
+++ WatermelonProject/Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+	public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+	{
+		protected static Singleton<T> _instance;
+
+		public static T Instance
+		{
+			get
+			{
+				if (_instance == null) {
+					var findObjectsOfType = FindObjectsOfType(typeof(T));
+					if (findObjectsOfType.Length > 0) {
+						var audioSequencer = (T)findObjectsOfType[0];
+						audioSequencer.Awake();
+					}
+				}
+				return _instance as T;
+			}
+		}
+
+		protected virtual void Awake()
+		{
+			if (_instance != null) {
+				Debug.Log(string.Format("Multiple instances of script {0}!", GetType()), gameObject);
+				Debug.Log("Other instance: " + _instance, _instance);
+			} else {
+				_instance = this;
+			}
+		}
+
+		protected virtual void OnDestroy()
+		{
+			_instance = null;
+		}
+	}
+}
